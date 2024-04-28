28011b6bf181b1b3ff82e132d00089fdd1accfc3
rizkyrsyd28 <rizkyrasyid28@gmail.com>
2024-04-28 13:14:49 +0700

    feat: add main character movement, aim, pair with gun

---
+++ Assets/Scripts/Player/Movement/MovementStateManager.cs
@@ -0,0 +1,80 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovementStateManager : MonoBehaviour
+{
+    public float currentMoveSpeed;
+    public float walkSpeed = 3, walkBackSpeed = 2;
+
+
+    [HideInInspector] public Vector3 dir;
+    public float hzInput, vtInput;
+    CharacterController controller; 
+    [SerializeField] float groundYOffset;
+    [SerializeField] LayerMask groundMask;
+    Vector3 spherePos;
+    [SerializeField] float gravity = -9.81f;
+    Vector3 velocity;
+
+    MovementBaseState currentState;
+    public IdleState idle = new IdleState(); 
+    public WalkState walk = new WalkState();
+    public RunState run = new RunState();
+
+    [HideInInspector] public Animator animator;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+        controller = GetComponent<CharacterController>();
+        SwitchState(idle);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        GetDirectionAndMove();
+        Gravity();
+
+        animator.SetFloat("hzInput", hzInput);
+        animator.SetFloat("vtInput", vtInput);
+
+        currentState.UpdateState(this);
+    }
+
+    public void SwitchState(MovementBaseState state) {
+        currentState = state;
+        currentState.EnterState(this);
+    }
+
+    void GetDirectionAndMove(){
+        hzInput = Input.GetAxis("Horizontal");
+        vtInput = Input.GetAxis("Vertical");
+
+        dir = transform.forward * vtInput + transform.right * hzInput;
+
+        controller.Move(dir.normalized * currentMoveSpeed * Time.deltaTime);
+    }
+
+    bool IsGrounded()
+    {
+        spherePos = new Vector3(transform.position.x, transform.position.y - groundYOffset, transform.position.z);
+        if (Physics.CheckSphere(spherePos, controller.radius - 0.05f, groundMask)) return true;
+        return false;
+    }
+
+    void Gravity()
+    {
+        if (!IsGrounded()) velocity.y += gravity * Time.deltaTime;
+        else if (velocity.y < 0) velocity.y = -2;
+
+        controller.Move(velocity * Time.deltaTime);
+    }
+    private void OnDrawGizmos() 
+    {
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireSphere(spherePos, controller.radius - 0.05f);
+    }
+}
