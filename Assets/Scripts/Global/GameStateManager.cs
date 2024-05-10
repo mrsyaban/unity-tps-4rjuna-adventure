b6838da6698145b8e83f643925a6af7fd7d2620c
0xzre <alilo.ghazali@gmail.com>
2024-05-10 11:14:09 +0700

    main menu settings and statistics logic

--- Assets/Scripts/Global/GameStateManager.cs
+++ Assets/Scripts/Global/GameStateManager.cs
@@ -4,6 +4,18 @@ public class GameStateManager : MonoBehaviour
 {
     public static GameStateManager Instance;
     public int coin;
+    // BUAT STATISTIK
+    public int bulletShot;
+    public int bulletHit;
+    public int distanceTraveled;
+    public int playtime;
+    public int krocoKilled;
+    public int kepalaKrocoKilled;
+    public int JendralKrocoKilled;
+    // BUAT SETTINGS
+    public string playerName;
+    public string difficulty;
+    public float gameVolume;
 
     private void Awake()
     {
@@ -17,9 +29,61 @@ public class GameStateManager : MonoBehaviour
         DontDestroyOnLoad(gameObject);
     }
 
+    public void LoadGameState()
+    {
+        // Nanti diisi list saved game kaya punya kinan krn gabisa nilai awal diinisialisasi lewat editor
+    }
+
     public void AddCoin(int val)
     {
         coin += val;
+    }
+
+    public void SetPlayerName(string name)
+    {
+        playerName = name;
+    }
+
+    public void SetDifficulty(string diff)
+    {
+        difficulty = diff;
+    }
+
+    public void SetGameVolume(float vol)
+    {
+        gameVolume = vol;
+    }
+
+    public void IncreaseGameVolume(int vol)
+    {
+        if(gameVolume + vol > 100)
+        {
+            gameVolume = 100;
+        }
+        else if(gameVolume + vol < 0)
+        {
+            gameVolume = 0;
+        }
+        else
+        {
+            gameVolume += vol;
+        }
+    }
+
+    public void DecreaseGameVolume(int vol)
+    {
+        if(gameVolume - vol > 100)
+        {
+            gameVolume = 100;
+        }
+        else if(gameVolume - vol < 0)
+        {
+            gameVolume = 0;
+        }
+        else
+        {
+            gameVolume -= vol;
+        }
     }
     
 }
