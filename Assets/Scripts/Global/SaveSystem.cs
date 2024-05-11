1c21e39762b8ace1f5f15ee2acc7d7c243da265d
irsyadnb <irsyadxd@gmail.com>
2024-05-11 11:18:49 +0700

    feat: save in game

--- Assets/Scripts/Global/SaveSystem.cs
+++ Assets/Scripts/Global/SaveSystem.cs
@@ -1,13 +1,15 @@
 using UnityEngine;
 using System.IO;
+using System;
 using UnityEngine.SceneManagement;
+using System.Linq;
 
 public static class SaveSystem
 {
     private static readonly string SAVE_FOLDER = Application.dataPath + "/Saves/";
     private static readonly string FILE_EXTENSION = ".json";
 
-    public static void SaveGameState()
+    public static void SaveGameState(string name)
     {
         // Create the save directory if it doesn't exist
         if (!Directory.Exists(SAVE_FOLDER))
@@ -19,7 +21,7 @@ public static class SaveSystem
         string json = JsonUtility.ToJson(GameStateManager.Instance);
 
         // Save the JSON data to a file
-        string filePath = SAVE_FOLDER + System.DateTime.Now.ToString("yyyy-MM-dd-HH-mm-ss") + FILE_EXTENSION;
+        string filePath = SAVE_FOLDER + name + "_" + System.DateTime.Now.ToString("yyyy-MM-dd-HH-mm-ss") + FILE_EXTENSION;
         File.WriteAllText(filePath, json);
 
         Debug.Log("Game state saved to: " + filePath);
@@ -44,5 +46,29 @@ public static class SaveSystem
         {
             Debug.LogWarning("No save file found at: " + filePath);
         }
+    }
+
+    public static string[] LoadRecentSaveSlots()
+    {
+        string[] files = Directory.GetFiles(SAVE_FOLDER)
+                            .Where(file => IsSaveFile(file))
+                            .OrderByDescending(file => GetDateTimeFromFileName(file))
+                            .Take(3)
+                            .ToArray();
+
+        return files;
+    }
+
+    private static bool IsSaveFile(string filePath)
+    {
+        string fileName = Path.GetFileName(filePath);
+        return fileName.EndsWith(".json") && fileName.Split("_").Length == 2;
+    }
+
+    private static DateTime GetDateTimeFromFileName(string fileName)
+    {
+        string[] parts = fileName.Split('_');
+        string dateTimeString = Path.GetFileNameWithoutExtension(parts[1]);
+        return DateTime.ParseExact(dateTimeString, "yyyy-MM-dd-HH-mm-ss", null);
     }
 }
